83510a96107b7de42e4a7f9b0ca3a7688a5f89dd
Dilan Patel <patel_dilan@hotmail.com>
2023-04-03 08:10:00 +0100

    Added error catch in NewsClient 'fetchHeadlines' method

--- newsClient.js
+++ newsClient.js
@@ -3,13 +3,21 @@ const apiKey = require('./apiKey');
 class NewsClient {
 
   async fetchHeadlines() {
-    const today = new Date().toISOString().slice(0, 10);
-    const url = `https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&from-date=${today}&api-key=${apiKey}`;
+    try {
+      // creates and converts date/time and then selects only the first part (date only)
+      const today = new Date().toISOString().slice(0, 10);
+      // added 'today' parameter
+      const url = `https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&from-date=${today}&api-key=${apiKey}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+      const response = await fetch(url);
+      const data = await response.json();
 
-    return data.response.results;
+      // easier access to desired properties such as headline
+      return data.response.results;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error fetching headlines');
+    }
   }
 
 };
